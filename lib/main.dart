11c6edcf72efb75f17341a736aa467fd720dd227
Lucas Franco <lucasfranco@192.168.100.20>
2025-10-28 20:41:08 -0300

    🔥 Configure Firebase with FlutterFire CLI
    - Add firebase_options.dart with platform configs
    - Configure Firebase for Web, Android, and iOS
    - Update main.dart to use DefaultFirebaseOptions
    - Add Firebase project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'core/constants/firebase_config.dart';
+import 'firebase_options.dart';
 import 'shared/theme/app_theme.dart';
 import 'core/router/app_router.dart';
 
@@ -9,15 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: FirebaseConfig.apiKey,
-      authDomain: FirebaseConfig.authDomain,
-      projectId: FirebaseConfig.projectId,
-      storageBucket: FirebaseConfig.storageBucket,
-      messagingSenderId: FirebaseConfig.messagingSenderId,
-      appId: FirebaseConfig.appId,
-      measurementId: FirebaseConfig.measurementId,
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(
